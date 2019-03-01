acbd856b5c66a670fdcad4bb08b41d30bff3c4f0
Chelsea Cui <acui@andrew.cmu.edu>
2019-03-01 00:58:50 -0500

    add full db schema, seed data for user/student/company

--- scripts/db/migrations/create_tables-20190228.sql
+++ scripts/db/migrations/create_tables-20190228.sql
@@ -2,6 +2,13 @@
 
 DROP TABLE IF EXISTS users;
 DROP TABLE IF EXISTS students;
+DROP TABLE IF EXISTS companies;
+DROP TABLE IF EXISTS skills;
+DROP TABLE IF EXISTS student_skills;
+DROP TABLE IF EXISTS tags;
+DROP TABLE IF EXISTS student_tags;
+DROP TABLE IF EXISTS positions;
+DROP TABLE IF EXISTS position_skills;
 
 CREATE TABLE IF NOT EXISTS users (
   id BIGSERIAL NOT NULL,
@@ -10,11 +17,11 @@ CREATE TABLE IF NOT EXISTS users (
   phone VARCHAR(50),
   role_type BOOLEAN DEFAULT true,
   avatar VARCHAR(255),
-  PRIMARY KEY ("id")
+  PRIMARY KEY (id)
 );
 
 CREATE TABLE IF NOT EXISTS students (
-  id BIGSERIAL,
+  id BIGSERIAL NOT NULL,
   user_id BIGSERIAL, 
   first_name VARCHAR(50) NOT NULL,
   last_name VARCHAR(50) NOT NULL,
@@ -25,7 +32,71 @@ CREATE TABLE IF NOT EXISTS students (
   major_1 VARCHAR(50) NOT NULL,
   major_2 VARCHAR(50),
   resume VARCHAR(255),
-  PRIMARY KEY ("id"),
-  FOREIGN KEY ("user_id") REFERENCES users("id") on delete cascade on update cascade
+  PRIMARY KEY (id),
+  FOREIGN KEY (user_id) REFERENCES users(id) on delete cascade on update cascade
 );
 
+CREATE TABLE IF NOT EXISTS companies (
+  id BIGSERIAL NOT NULL,
+  user_id BIGSERIAL, 
+  name VARCHAR(255) NOT NULL,
+  location TEXT[],
+  banner_img VARCHAR(255),
+  description TEXT,
+  contact_name VARCHAR(50) NOT NULL,
+  active BOOLEAN DEFAULT true,
+  PRIMARY KEY (id),
+  FOREIGN KEY (user_id) REFERENCES users(id) on delete cascade on update cascade
+);
+
+CREATE TABLE IF NOT EXISTS skills (
+  id BIGSERIAL NOT NULL,
+  category VARCHAR(50) NOT NULL,
+  name VARCHAR(50) NOT NULL,
+  PRIMARY KEY (id)
+);
+
+CREATE TABLE IF NOT EXISTS student_skills (
+  student_id BIGSERIAL NOT NULL, 
+  skill_id BIGSERIAL NOT NULL,
+  PRIMARY KEY (student_id, skill_id),
+  FOREIGN KEY (student_id) REFERENCES students(id) on delete cascade on update cascade,
+  FOREIGN KEY (skill_id) REFERENCES skills(id) on delete cascade on update cascade
+);
+
+CREATE TABLE IF NOT EXISTS tags (
+  id BIGSERIAL NOT NULL,
+  company_id BIGSERIAL NOT NULL,
+  name VARCHAR(50) NOT NULL,
+  PRIMARY KEY (id),
+  FOREIGN KEY (company_id) REFERENCES companies(id) on delete cascade on update cascade
+);
+
+CREATE TABLE IF NOT EXISTS student_tags (
+  student_id BIGSERIAL NOT NULL, 
+  tag_id BIGSERIAL NOT NULL,
+  PRIMARY KEY (student_id, tag_id),
+  FOREIGN KEY (student_id) REFERENCES students(id) on delete cascade on update cascade,
+  FOREIGN KEY (tag_id) REFERENCES tags(id) on delete cascade on update cascade
+);
+
+CREATE TABLE IF NOT EXISTS positions (
+  id BIGSERIAL NOT NULL,
+  company_id BIGSERIAL NOT NULL,
+  name VARCHAR(128) NOT NULL,
+  job_type VARCHAR(50) NOT NULL,
+  location TEXT[],
+  application_deadline DATE,
+  description TEXT,
+  active BOOLEAN DEFAULT true,
+  PRIMARY KEY (id),
+  FOREIGN KEY (company_id) REFERENCES companies(id) on delete cascade on update cascade
+);
+
+CREATE TABLE IF NOT EXISTS position_skills (
+  position_id BIGSERIAL NOT NULL, 
+  skill_id BIGSERIAL NOT NULL,
+  PRIMARY KEY (position_id, skill_id),
+  FOREIGN KEY (position_id) REFERENCES positions(id) on delete cascade on update cascade,
+  FOREIGN KEY (skill_id) REFERENCES skills(id) on delete cascade on update cascade
+);
--- scripts/db/seeds/add_users-20190228.sql
+++ scripts/db/seeds/add_users-20190228.sql
@@ -7,9 +7,19 @@ INSERT INTO users (email, passwd, phone) VALUES
     '4121231234'
   ),
   (
-    'acui@andrew.cmu.edu',
+    'aiqi@gmail.com',
     'secret-123',
-    '8148829127'
+    '4121234321'
+  ),
+  (
+    'google@gmail.com',
+    'secret-123',
+    '4121230000'
+  ),
+  (
+    'linkedin@gmail.com',
+    'secret-123',
+    '4121231111'
   );
 
 INSERT INTO students (user_id, first_name, last_name, school, degree, graduation_year, major_1) VALUES
@@ -30,4 +40,18 @@ INSERT INTO students (user_id, first_name, last_name, school, degree, graduation
     0,
     2020,
     'Information Systems'
+  );
+
+INSERT INTO companies (user_id, name, contact_name, description) VALUES
+  (
+    3,
+    'Google',
+    'Laura Doe',
+    'Google is a good company!'
+  ),
+  (
+    4,
+    'LinkedIn',
+    'William Linderson',
+    'LinkedIn is cool!'
   );
