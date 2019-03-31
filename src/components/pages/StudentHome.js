9df078c04b08099c9f7169182448aa07a3de41c1
Xinhe Zhang <xinhez@duolingo.com>
2019-03-31 10:45:21 -0400

    Delete Master Frontend code

--- src/components/pages/StudentHome.js
+++
@@ -1,9 +0,0 @@
-import React, { Component } from "react";
-
-class StudentHome extends Component {
-  render() {
-    return <div>Student Home</div>;
-  }
-}
-
-export default StudentHome;
--- src/reducers/rootReducer.js
+++ src/reducers/rootReducer.js
@@ -1,9 +1,7 @@
 import { combineReducers } from "redux";
 
-import studentReducer from "./student";
 import companyReducer from "./company";
 
 export default combineReducers({
-  student: studentReducer,
   company: companyReducer
 });
